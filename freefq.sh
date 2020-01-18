fc653b47674d1f49b3d21fc88245ef018a8afd26
chnroute <chnroute@gmail.com>
2020-01-18 04:22:53 +0000

    Add .travis.yml and freefq.sh

---
+++ freefq.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+type=$1
+[ -n "$type" ] || type=ss
+date=$2
+[ -n "$date" ] && date=$(date '+%s' -d$date)
+[ -n "$date" ] || date=$(date '+%s')
+
+
+
+urlhome=https://www.freefq.com
+case $type in
+    ss|ssr) path=free-$type;;
+    v2ray) path=$type;;
+    *) echo unknown $type; exit 1;;
+esac
+url=$urlhome/$path/
+wget -q $url -O index.txt
+path=$(iconv -f gbk -t utf8 index.txt | dos2unix | sed 's/[<>]/\n/g' | \
+    grep 'href="/'$path'/.*\.html' | sed -E 's/^.*href="([^"]*)".*/\1/' | \
+    grep -P '\d{4}/\d{2}/\d{2}' | sort -ru | head -n 1)
+url=$urlhome/$path
+rm -f index.txt
+wget -q $url -O index.txt
+url=$(iconv -f gbk -t utf8 index.txt | dos2unix | grep 'href=.*\.txt'|cut -d\" -f 2)
+rm -f index.txt
+wget -q $url -O index.txt
+iconv -f gbk -t utf8 index.txt | dos2unix | grep -P '\t' | grep -Ev '^账号|^ssr链接|^Address' | sed -E 's/ \t/|/g' > $type-list.txt
+mv -f index.txt orig-$type-list.txt
