9e31215a4869b5ef630ad62e05621eee58ced705
chnroute <chnroute@gmail.com>
2020-12-22 08:27:49 +0000

    split v2ray to vmess and trojan
    Signed-off-by: chnroute <chnroute@gmail.com>

--- freefq.sh
+++ freefq.sh
@@ -29,7 +29,10 @@ rm -f index.txt
 wget -q $url -O index.txt
 iconv -f gbk -t utf8 index.txt | dos2unix >index1.txt
 mv -f index1.txt index.txt
-#grep -P '\t|(ss|ssr|vmess|trojan)://[\x20-\x7f]' index.txt | grep -Ev '^账号|^ssr链接|^Address' | sed -E 's/ \t/|/g' > $type-list.txt
 sed -r -e 's@[<>]@\n@g' index.txt | grep -P '(ss|ssr|vmess|trojan)://[\x20-\x7f]' | grep -Ev '^账号|^ssr链接|^Address' | sed -E 's/ \t/|/g' > $type-list.txt
 sed -E -i -e 's@<.*data="@@' -e 's@".*>@@' $type-list.txt
+if [ "$type" = "v2ray" ]; then
+    grep -P '^vmess' $type-list.txt > vmess-list.txt
+    grep -P '^trojan' $type-list.txt > trojan-list.txt
+fi
 mv -f index.txt orig-$type-list.txt
