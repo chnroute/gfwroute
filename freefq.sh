1fdbe5c5e39ca68d550df61edb493b8eed886e41
chnroute <chnroute@gmail.com>
2020-12-25 06:07:11 +0000

    add timeout and progress options for wget
    Signed-off-by: chnroute <chnroute@gmail.com>

--- freefq.sh
+++ freefq.sh
@@ -6,7 +6,7 @@ date=$2
 [ -n "$date" ] && date=$(date '+%s' -d$date)
 [ -n "$date" ] || date=$(date '+%s')
 
-
+wgetopt='-q --dns-timeout=10 --connect-timeout=10 --read-timeout=30 --tries=3 --show-progress --progress=dot'
 
 urlhome=https://www.freefq.com
 case $type in
@@ -15,18 +15,21 @@ case $type in
     *) echo unknown $type; exit 1;;
 esac
 url=$urlhome/$path/
-wget -q $url -O index.txt
+wget $wgetopt $url -O index.txt || exit 0
+echo
 path=$(iconv -f gbk -t utf8 index.txt | dos2unix | sed 's/[<>]/\n/g' | \
     grep 'href="/'$path'/.*\.html' | sed -E 's/^.*href="([^"]*)".*/\1/' | \
     grep -P '\d{4}/\d{2}/\d{2}' | sort -ru | head -n 1)
 url=$urlhome/$path
 rm -f index.txt
-wget -q $url -O index.txt
+wget $wgetopt $url -O index.txt || exit 0
+echo
 iconv -f gbk -t utf8 index.txt | dos2unix >index1.txt
 mv -f index1.txt index.txt
 url=$(grep -P 'href=.*file/(free-ss|free-ssr|v2ray).*\.htm' index.txt|cut -d\" -f 2)
 rm -f index.txt
-wget -q $url -O index.txt
+wget $wgetopt $url -O index.txt || exit 0
+echo
 iconv -f gbk -t utf8 index.txt | dos2unix >index1.txt
 mv -f index1.txt index.txt
 sed -r -e 's@[<>]@\n@g' index.txt | grep -P '(ss|ssr|vmess|trojan)://[\x20-\x7f]' | grep -Ev '^账号|^ssr链接|^Address' | sed -E 's/ \t/|/g' > $type-list.txt
@@ -37,3 +40,4 @@ if [ "$type" = "v2ray" ]; then
     grep -P '^trojan' $type-list.txt > trojan-list.txt
 fi
 mv -f index.txt orig-$type-list.txt
+echo ok
