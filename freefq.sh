72cecba0825417efc29096225e98d6e692970069
chnroute <chnroute@gmail.com>
2021-01-02 04:37:39 +0000

    add scripts to detect location
    Signed-off-by: chnroute <chnroute@gmail.com>

--- freefq.sh
+++ freefq.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+rootdir=$(dirname $0)
+
 type=$1
 [ -n "$type" ] || type=ss
 date=$2
@@ -7,6 +9,8 @@ date=$2
 [ -n "$date" ] || date=$(date '+%s')
 
 wgetopt='-q --dns-timeout=10 --connect-timeout=10 --read-timeout=30 --tries=3 --show-progress --progress=dot'
+
+urlgeoip="https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-City&license_key=$LICENSE_KEY&suffix=tar.gz"
 
 urlhome=https://www.freefq.com
 case $type in
@@ -34,10 +38,20 @@ iconv -f gbk -t utf8 index.txt | dos2unix >index1.txt
 mv -f index1.txt index.txt
 sed -r -e 's@[<>]@\n@g' index.txt | grep -P '(ss|ssr|vmess|trojan)://[\x20-\x7f]' | grep -Ev '^账号|^ssr链接|^Address' | sed -E 's/ \t/|/g' > $type-list.txt
 sed -E -i -e 's@<.*data="@@' -e 's@".*>@@' $type-list.txt
-base64 $type-list.txt > subscribe-$type.txt
 if [ "$type" = "v2ray" ]; then
+    if [ ! -f geoip.tar.gz ]; then
+        wget $wgetopt $urlgeoip -O geoip.tar.gz
+    fi
+    if [ ! -f ./GeoLite2-City/GeoLite2-City.mmdb ]; then
+        tar xf geoip.tar.gz --transform='s/_[0-9]*//g'  --wildcards '*.mmdb'
+    fi
+    if [ -f ./GeoLite2-City/GeoLite2-City.mmdb ]; then
+        python3 $rootdir/getcity.py $type-list.txt > $type-list1.txt
+        mv $type-list1.txt $type-list.txt
+    fi
     grep -P '^vmess' $type-list.txt > vmess-list.txt
     grep -P '^trojan' $type-list.txt > trojan-list.txt
 fi
+base64 $type-list.txt > subscribe-$type.txt
 mv -f index.txt orig-$type-list.txt
 echo ok
---
+++ getcity.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+
+import sys
+import json
+import socket
+import base64
+import pprint
+import urllib.parse
+import geoip2.database
+
+def get_city(addr):
+    iplist = list({addr[-1][0] for addr in socket.getaddrinfo(addr, 0, 0, 0, 0)})
+    if len(iplist) < 1:
+        exit(1)
+    addr = iplist.pop()
+
+    client = geoip2.database.Reader(r'./GeoLite2-City/GeoLite2-City.mmdb')
+    try:
+        response = client.city(addr)
+    except geoip2.errors.AddressNotFoundError:
+        return '未知'
+
+    city = response.country.names['zh-CN']
+
+    if len(response.subdivisions) > 0:
+        if 'zh-CN' in response.subdivisions[0].names:
+            city += ' ' + response.subdivisions[0].names['zh-CN']
+        else:
+            city += ' ' + response.subdivisions[0].names['en']
+
+    if response.city.names:
+        if 'zh-CN' in response.city.names:
+            city += ' ' + response.city.names['zh-CN']
+        else:
+            city += ' ' + response.city.names['en']
+    return city
+
+def decode_trojan(text):
+    pos = text.find('@')
+    pos2 = text.find(':')
+    addr = text[pos +1:pos2]
+    city = get_city(addr)
+    pos = text.find('#')
+    print('trojan://' + text[:pos] + '#' + urllib.parse.quote(city))
+    pass
+
+def decode_vmess(text):
+    code = base64.b64decode(text).decode('UTF-8')
+    jo = json.loads(code)
+    #pprint.pprint(jo['add'])
+    jo['ps'] = get_city(jo['add'])
+    #pprint.pprint(jo['ps'])
+    print('vmess://' + base64.b64encode(json.dumps(jo).encode('UTF-8')).decode('UTF-8'))
+    pass
+
+def main():
+    for filename in sys.argv[1:]:
+        fo = open(filename, 'r')
+        while True:
+            line = fo.readline()
+            if not line:
+                break
+            pos = line.find('://')
+            if pos < 0:
+                continue
+            schema = line[:pos]
+            if schema == 'trojan':
+                decode_trojan(line[pos+3:])
+            elif schema == 'vmess':
+                decode_vmess(line[pos+3:])
+            else:
+                pass
+        fo.close()
+    pass
+
+if __name__ == '__main__':
+    main()
+
+
+
